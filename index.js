0ae0cf631e5e8e187f64379e1131b7e2ce49e3b9
jml-student <student741@proton.me>
2024-07-15 20:50:47 +0200

    Add menu styles, create img folder and import images

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { displayMenu } from './node_modules/display-menu-package/index.mjs';
+import { displayMenu } from './node_modules/display-menu-package/index.js';
 
 (function getItems() {
     const menuButton = document.querySelector('.menu-btn');
     const menuContainer = document.querySelector('.menu-container');
+    menuContainer.style.visibility = 'hidden';
     displayMenu(menuButton, menuContainer);
 })();
