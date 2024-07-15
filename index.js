52e25520aa67e0876faf23fafa845ac72d0e0188
jml-student <student741@proton.me>
2024-07-15 22:29:47 +0200

    Create slider dots, apply more styles and create slider 5 second interval functionality

--- index.js
+++ index.js
@@ -1,21 +1,76 @@
 import { displayMenu } from './node_modules/display-menu-package/index.js';
 
-(function getItems() {
-    const menuButton = document.querySelector('.menu-btn');
-    const menuContainer = document.querySelector('.menu-container');
-    menuContainer.style.visibility = 'hidden';
-    displayMenu(menuButton, menuContainer);
-})();
+(function main() {
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
+    const dots = document.querySelectorAll('.dot');
+    let carouselIndex = 0;
 
-function displayCarousel(index) {
-    const images = document.querySelectorAll('.carousel-container img');
-    images.forEach((image, currentImage) => {
-        if (currentImage === index) {
-            image.style.display = 'block';
+    function getMenu() {
+        const menuButton = document.querySelector('.menu-btn');
+        const menuContainer = document.querySelector('.menu-container');
+        menuContainer.style.visibility = 'hidden';
+        displayMenu(menuButton, menuContainer);
+    }
+
+    function displayCarousel(index) {
+        const images = document.querySelectorAll('.carousel-container img');
+        images.forEach((image, currentImage) => {
+            if (currentImage === index) {
+                image.style.display = 'block';
+            } else {
+                image.style.display = 'none';
+            }
+        });
+        applyDotsBgColor();
+    }
+
+    function applyDotsBgColor() {
+        const dots = document.querySelectorAll('.dot');
+        dots.forEach((dot, index) => {
+            if (index === carouselIndex) {
+                dot.style.backgroundColor = 'rgb(184, 184, 184)';
+            } else {
+                dot.style.backgroundColor = 'white';
+            }
+        });
+    }
+
+    function sumOneIndex() {
+        if (carouselIndex === 7) {
+            carouselIndex = 0;
         } else {
-            image.style.display = 'none';
+            carouselIndex += 1;
         }
-    });
-}
+        displayCarousel(carouselIndex);
+    }
 
-displayCarousel(0);
+    leftArrow.addEventListener('click', () => {
+        if (carouselIndex === 0) {
+            carouselIndex = 7;
+        } else {
+            carouselIndex -= 1;
+        }
+        displayCarousel(carouselIndex);
+    });
+
+    rightArrow.addEventListener('click', () => {
+        if (carouselIndex === 7) {
+            carouselIndex = 0;
+        } else {
+            carouselIndex += 1;
+        }
+        displayCarousel(carouselIndex);
+    });
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            carouselIndex = index;
+            displayCarousel(carouselIndex);
+        });
+    });
+
+    getMenu();
+    displayCarousel(carouselIndex);
+    setInterval(sumOneIndex, 5000);
+})();
